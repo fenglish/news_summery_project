3c56c966b550f2beb128090343b6f460a4ed3739
Asuka Ochi <asukaochi@gmail.com>
2017-01-07 20:20:31 +0000

    add get functions in News model

--- src/models/news-model.js
+++ src/models/news-model.js
@@ -7,6 +7,22 @@
     this.summary = ""
   }
 
+  News.prototype.getHeadline = function(){
+    return this.headline
+  };
+
+  News.prototype.getSourceURL = function(){
+    return this.sourceURL
+  };
+
+  News.prototype.getImageURL = function(){
+    return this.imageURL
+  };
+
+  News.prototype.getSummary = function(){
+    return this.summary
+  };
+
   exports.News = News;
 
 })(this);
