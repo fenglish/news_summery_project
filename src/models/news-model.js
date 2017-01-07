c380a990ebd338c1ff7aca2d88388646f94d8642
Asuka Ochi <asukaochi@gmail.com>
2017-01-07 20:05:45 +0000

    add News Model with property

---
+++ src/models/news-model.js
@@ -0,0 +1,12 @@
+(function(exports){
+
+  function News(){
+    this.headline = ""
+    this.sourceURL = ""
+    this.imageURL = ""
+    this.summary = ""
+  }
+
+  exports.News = News;
+
+})(this);
