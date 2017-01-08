fff2f5abba5a1a3851c8c1f0091f34b43307bc76
Asuka Ochi <asukaochi@gmail.com>
2017-01-08 00:04:57 +0000

    delete a functon haven't used

--- src/views/news-list-view.js
+++ src/views/news-list-view.js
@@ -4,10 +4,6 @@
     this.list = newslist
   }
 
-  NewsListView.prototype.getList = function(){
-      return this.list
-  };
-
   NewsListView.prototype.listNews = function(){
       var allNews = this.list.getAllNews()
       var listInHtml = allNews.map(function(news){
